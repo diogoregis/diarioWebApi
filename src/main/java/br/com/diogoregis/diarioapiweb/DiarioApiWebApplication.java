eb6809b20462f8322cbcb7425908bd4902cd832c
Diogo Regis <diogo.regis.silva@gmail.com>
2024-08-05 11:45:53 -0300

    initial

---
+++ src/main/java/br/com/diogoregis/diarioapiweb/DiarioApiWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.diogoregis.diarioapiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioApiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiarioApiWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/diogoregis/diarioapiweb/DiarioApiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diogoregis.diarioapiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioApiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
