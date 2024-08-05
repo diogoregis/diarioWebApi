4c52d460d05b30fa6abcafc8efb919c7944d7e02
Diogo Regis <diogo.regis.silva@gmail.com>
2024-08-05 12:44:16 -0300

    criado service na class RegistroService

---
+++ src/main/java/br/com/diogoregis/diarioapiweb/service/RegistroService.java
@@ -0,0 +1,39 @@
+package br.com.diogoregis.diarioapiweb.service;
+
+import br.com.diogoregis.diarioapiweb.model.Registro;
+import br.com.diogoregis.diarioapiweb.repository.RegistroRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RegistroService {
+
+    private RegistroRepository repository;
+
+    public RegistroService(RegistroRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Registro> list(){
+        Sort sort = Sort.by("data").descending();
+        return repository.findAll(sort);
+    }
+
+    public Registro save (Registro registro){
+        return repository.save(registro);
+    }
+
+    public Optional<Registro> findById(Long id){
+        return repository.findById(id);
+    }
+
+    public Registro put(Registro registro){
+        if(repository.existsById(registro.getId())){
+            return save(registro);
+        }
+        return null;
+    }
+}
